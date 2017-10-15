958bdd028a0a614f9a3dcffbc62ae25e47adc8c0
System Administrator <root@acanter-m-14r8.home>
2017-10-15 16:36:50 -0500

    Adding Module 2

---
+++ Multisensor_Road_Trip.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+#
+# Pi-Athlon Module 2 - Multisensors Road Trip
+# Adds RGB LED, buzzer, obstacle detector, flame/heat detector
+#
+# Following black line, no obstacle detected -> Green LED
+# Detected object -> Stop -> Red LED -> wait 5s -> buzz until obstacle removed -> green LED and proceed forward
+# Detected heat >28c -> Stop -> Blue LED -> wait 10s -> ?? wait 10 more seconds ?? -> green LED and proceed forward
+#
+
+import RobotInverse
+import RobotGPIO
+import time
+
+def detectedObstacle():
+  if(RobotGPIO.detectObstacle() == 1):
+    RobotGPIO.redOn()
+    time.sleep(5)
+    RobotGPIO.buzzOn()
+    while(RobotGPIO.detectObstacle()):
+      #mmm drawing a blank
+    RobotGPIO.greenOn()
+
+def detectedFlame():
+  if(RobotGPIO.detectFlame() == 1):
+    RobotGPIO.blueOn()
+    time.sleep(20)
+    RobotGPIO.greenOn()
+
+
