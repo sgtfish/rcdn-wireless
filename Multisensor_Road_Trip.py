244f09ca05089af9c97ed4b865b3c26577116c28
ahowcan <austincanter@yahoo.com>
2017-10-23 22:56:00 -0500

    Added DS18B20 temp reading code

--- Multisensor_Road_Trip.py
+++ Multisensor_Road_Trip.py
@@ -11,6 +11,8 @@
 import RobotInverse
 import RobotGPIO
 import time
+import os
+import glob
 
 def detectedObstacle():
   Obstacle_Detected = False
@@ -23,6 +25,39 @@ def detectedObstacle():
       # Returns 1 if obsatcle detected
       Obstacle_Detected = False
   RobotGPIO.greenOn()
+
+#-----------Temp Read-----------------------------
+# Used this for reference
+# Still need to do inital config, see link below
+# https://cdn-learn.adafruit.com/downloads/pdf/adafruits-raspberry-pi-lesson-11-ds18b20-temperature-sensing.pdf
+#
+
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
+
+base_dir = '/sys/bus/w1/devices/'
+device_folder = glob.glob(base_dir + '28*')[0]
+device_file = device_folder + '/w1_slave'
+
+def read_temp_raw():
+  f = open(device_file, 'r')
+  lines = f.readlines()
+  f.close()
+  return lines
+
+def read_temp():
+  lines = read_temp_raw()
+  while lines[0].strip()[-3:] != 'YES':
+    time.sleep(0.2)
+    lines = read_temp_raw() 
+  equals_pos = lines[1].find('t=') 
+  if equals_pos != -1:
+    temp_string = lines[1][equals_pos+2:] 
+    temp_c = float(temp_string) / 1000.0 
+    temp_f = temp_c * 9.0 / 5.0 + 32.0 
+    return temp_c, temp_f
+
+---------------------------------------------------
 
 def detectedFlame():
   RobotGPIO.blueOn()
