bc3cd7c00389e6d64332f169d2745eb28ef910cd
rockyraccoonx <36086027+rockyraccoonx@users.noreply.github.com>
2018-02-03 23:35:20 -0600

    Update parse.py
    Added comments.

--- parse.py
+++ parse.py
@@ -4,16 +4,22 @@ import moveRobot
 
 def parseFile(fileName):
   syslogger.log("Parser","INFO","Opening file")
+  # This will break if a file is not found. I suggest handling file opening inside a 'try'.
   with open(fileName,'r') as json_file:
     try:
       syslogger.log("Parser","INFO","Begin parsing " + fileName) 
       cmds = json.load(json_file)
       cmd_count = len(cmds)
     except:
+      # If we get to this exception, execution will continue on the next 'try'.
+      # If we cannot decode the JSON objects from the file, should we continue to compile the commands?
+      # Perhaps this can be handled in main, but then something has to be returned here.
       syslogger.log("Parser","ERROR","Could not find JSON objects")
   
     try:
-      syslogger.log("Parser","INFO","Compiling commands")
+      # This stage suggests that we have the JSON data ready. Is that true?
+      # Both "Could not find JSON objects" and "Compiling commands" will get logged. Is that possible? 
+      syslogger.log("Parser","INFO","Compiling commands") 
       for i in range(cmd_count):
         action = cmds[i]['action']
         time = cmds[i]['time']
@@ -21,4 +27,4 @@ def parseFile(fileName):
         syslogger.log("Parser","INFO","Sending action " + action + " for " + str(time) + " seconds.")
         moveRobot.command(action,time)
     except:
-      syslogger.log("Parser","ERROR","Failed to parse file")
+      syslogger.log("Parser","ERROR","Failed to parse file") 
