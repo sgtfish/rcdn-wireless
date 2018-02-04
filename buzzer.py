0338bc6a2e516c45199af00bfe2d362261baa1b5
sgtfish <sgt.fish@gmail.com>
2018-02-04 17:34:59 -0600

    added beep function

--- buzzer.py
+++ buzzer.py
@@ -6,3 +6,12 @@ def buzz(duration):
   time.sleep(duration)
   robotGPIO.buzzOff()
  
+def beep(duration)
+  INTERVAL = 0.25
+  while duration > 0:
+    buzzOn()
+    time.sleep(INTERVAL)
+    duration = duration - INTERVAL
+    buzzOff()
+    time.sleep(interval)
+    duration = duration - INTERVAL
