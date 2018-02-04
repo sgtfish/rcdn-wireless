8ecdc93defc9e53feea3a39e9b9ee7763fd86b67
sgtfish <sgt.fish@gmail.com>
2018-02-04 16:49:53 -0600

    cleaned up exceptions. "time" -> "duration"
    Per Joel's(?) comments.
    Can't test this right now, so might have logic/syntax errors

--- parse.py
+++ parse.py
@@ -4,27 +4,27 @@ import moveRobot
 
 def parseFile(fileName):
   syslogger.log("Parser","INFO","Opening file")
-  # This will break if a file is not found. I suggest handling file opening inside a 'try'.
-  with open(fileName,'r') as json_file:
     try:
-      syslogger.log("Parser","INFO","Begin parsing " + fileName) 
-      cmds = json.load(json_file)
-      cmd_count = len(cmds)
+      with open(fileName,'r') as json_file:
+        try:
+          syslogger.log("Parser","INFO","Begin parsing " + fileName) 
+          cmds = json.load(json_file)
+          cmd_count = len(cmds)
+      except:
+        syslogger.log("Parser","ERROR","Could not find JSON objects")
+        sys.exit(1)
     except:
-      # If we get to this exception, execution will continue on the next 'try'.
-      # If we cannot decode the JSON objects from the file, should we continue to compile the commands?
-      # Perhaps this can be handled in main, but then something has to be returned here.
-      syslogger.log("Parser","ERROR","Could not find JSON objects")
+      syslogger.log("Parser","ERROR","File not found in flash!")
+      sys.exit(1)
   
     try:
-      # This stage suggests that we have the JSON data ready. Is that true?
-      # Both "Could not find JSON objects" and "Compiling commands" will get logged. Is that possible? 
       syslogger.log("Parser","INFO","Compiling commands") 
       for i in range(cmd_count):
         action = cmds[i]['action']
-        time = cmds[i]['time']
+        duration = cmds[i]['duration']
 
-        syslogger.log("Parser","INFO","Sending action " + action + " for " + str(time) + " seconds.")
-        moveRobot.command(action,time)
+        syslogger.log("Parser","INFO","Sending action " + action + " for " + str(duration) + " seconds.")
+        moveRobot.command(action,duration)
     except:
       syslogger.log("Parser","ERROR","Failed to parse file") 
+      sys.exit(1)
